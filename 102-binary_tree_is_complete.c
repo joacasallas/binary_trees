2c91c69a764530b43405eb6447b39414a11b961a
joacasallas <joacasallas@gmail.com>
2022-03-25 10:38:11 -0500

    update

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,32 +1,55 @@
 #include "binary_trees.h"
-
 /**
-  * binary_tree_is_complete - checks if a binary tree is complete
-  * @tree: root node of the BT
-  * Return: 1 if is complete otherwise 0
-  */
+ * binary_tree_is_complete - is complete
+ * @tree: tree
+ * Return: 0 in false 1 in true
+ */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	size_t comp_l = 0, comp_r = 0, comp = 0;
+	unsigned int i = 0;
+	size_t size;
 
-	if (!tree || (!tree->left && tree->right))
+	if (tree == NULL)
 		return (0);
 
-	if (!(tree->left) && !(tree->right))
+	size = binary_tree_size(tree);
+	return (check(tree, i, size));
+}
+/**
+ * check - helper func for binary_tree_is_complete
+ * @tree: tree
+ * @index: current node
+ * @size: size
+ * Return: 0 in false 1 in true
+ */
+int check(const binary_tree_t *tree, unsigned int index, size_t size)
+{
+	int isComplete = 0;
+
+	if (tree == NULL)
 		return (1);
 
-	comp_l = binary_tree_is_complete(tree->left);
-	comp_r = binary_tree_is_complete(tree->right);
+	if (index >= size)
+		return (0);
 
-	if (tree->left && !tree->right && (comp_l == 1 && comp_r == 0))
-		return (2);
+	isComplete = check(tree->left, 2 * index + 1, size) &&
+		check(tree->right, 2 * index + 2, size);
+	return (isComplete);
+}
 
-	if ((comp_l == 0 && comp_r > 0) || (comp_l > 0 && comp_r == 0))
-		comp = 0;
-	else if ((comp_l == 2 && comp_r == 2) || (comp_l == 2 && comp_r == 2))
-		comp = 0;
-	else
-		comp = 1;
+/**
+ * binary_tree_size - measure the size
+ * @tree: input
+ * Return: size
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	int size;
 
-	return (comp);
+	if (tree == NULL)
+		return (0);
+
+	size = 1 + binary_tree_size(tree->right) + binary_tree_size(tree->left);
+
+	return (size);
 }
