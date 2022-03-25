d8fe949c288edf5d3600615775c5738d4b0c8f77
joacasallas <joacasallas@gmail.com>
2022-03-25 09:15:41 -0500

    update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,16 +9,16 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newleft_node = binary_tree_node(parent, value);
+	binary_tree_t *left_child;
 
-	if (parent == NULL || newleft_node == NULL)
-	{
+	if (parent == NULL)
 		return (NULL);
-	}
-	if (parent->left != NULL)
-	{
-		parent->left->left = parent->left;
-	}
-	parent->left = newleft_node;
-	return (newleft_node->left);
+	left_child = binary_tree_node_1(parent, value);
+	if (left_child == NULL)
+		return (NULL);
+	left_child->left = parent->left;
+	if (left_child->left != NULL)
+		left_child->left->parent = left_child;
+	parent->left = left_child;
+	return (left_child);
 }
